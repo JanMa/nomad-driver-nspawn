36a2288a700a8096891c47b39b688169161ba5b9
JanMa <git@janma.tk>
2019-10-20 11:58:25 +0200

    Make driver able to register with Nomad
    No starting or stopping of Tasks yet. Only registers with the Nomad agent
    on startup and is detected as healthy

--- nspawn/driver.go
+++ nspawn/driver.go
@@ -2,17 +2,25 @@ package nspawn
 
 import (
 	"context"
-	// "fmt"
+	"fmt"
+	"os/exec"
+	"strconv"
+	"strings"
 	"time"
 
 	hclog "github.com/hashicorp/go-hclog"
 	// "github.com/hashicorp/nomad/client/stats"
+	systemdDbus "github.com/coreos/go-systemd/dbus"
+	// machined "github.com/coreos/go-systemd/machine1"
+	systemdUtil "github.com/coreos/go-systemd/util"
+	// "github.com/godbus/dbus"
+	cstructs "github.com/hashicorp/nomad/client/structs"
 	"github.com/hashicorp/nomad/drivers/shared/eventer"
 	"github.com/hashicorp/nomad/plugins/base"
 	"github.com/hashicorp/nomad/plugins/drivers"
+	driversUtil "github.com/hashicorp/nomad/plugins/drivers/utils"
 	"github.com/hashicorp/nomad/plugins/shared/hclspec"
-	// pstructs "github.com/hashicorp/nomad/plugins/shared/structs"
-	cstructs "github.com/hashicorp/nomad/client/structs"
+	pstructs "github.com/hashicorp/nomad/plugins/shared/structs"
 )
 
 const (
@@ -34,6 +42,33 @@ var (
 		PluginApiVersions: []string{drivers.ApiVersion010},
 		PluginVersion:     "0.0.1",
 		Name:              pluginName,
+	}
+
+	// configSpec is the hcl specification returned by the ConfigSchema RPC
+	configSpec = hclspec.NewObject(map[string]*hclspec.Spec{
+		"enabled": hclspec.NewDefault(
+			hclspec.NewAttr("enabled", "bool", false),
+			hclspec.NewLiteral("true"),
+		),
+		// "volumes_enabled": hclspec.NewDefault(
+		// 	hclspec.NewAttr("volumes_enabled", "bool", false),
+		// 	hclspec.NewLiteral("true"),
+		// ),
+		// "lxc_path": hclspec.NewAttr("lxc_path", "string", false),
+	})
+
+	// taskConfigSpec is the hcl specification for the driver config section of
+	// a task within a job. It is returned in the TaskConfigSchema RPC
+	taskConfigSpec = hclspec.NewObject(map[string]*hclspec.Spec{
+		"image": hclspec.NewAttr("image", "string", true),
+	})
+
+	// capabilities is returned by the Capabilities RPC and indicates what
+	// optional features this driver supports
+	capabilities = &drivers.Capabilities{
+		SendSignals: false,
+		Exec:        false,
+		FSIsolation: drivers.FSIsolationImage,
 	}
 )
 
@@ -70,6 +105,7 @@ type Config struct {
 	Enabled bool `codec:"enabled"`
 }
 
+// NewNspawnDriver returns a new nspawn driver object
 func NewNspawnDriver(logger hclog.Logger) drivers.DriverPlugin {
 	ctx, cancel := context.WithCancel(context.Background())
 	logger = logger.Named(pluginName)
@@ -84,14 +120,60 @@ func NewNspawnDriver(logger hclog.Logger) drivers.DriverPlugin {
 }
 
 func (d *Driver) TaskConfigSchema() (*hclspec.Spec, error) {
-	return nil, nil
+	return taskConfigSpec, nil
 }
 func (d *Driver) Capabilities() (*drivers.Capabilities, error) {
-	return nil, nil
+	return capabilities, nil
 }
-func (d *Driver) Fingerprint(context.Context) (<-chan *drivers.Fingerprint, error) {
-	return nil, nil
+
+func (d *Driver) Fingerprint(ctx context.Context) (<-chan *drivers.Fingerprint, error) {
+	ch := make(chan *drivers.Fingerprint)
+	go d.handleFingerprint(ctx, ch)
+	return ch, nil
 }
+
+func (d *Driver) handleFingerprint(ctx context.Context, ch chan<- *drivers.Fingerprint) {
+	defer close(ch)
+	ticker := time.NewTimer(0)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-d.ctx.Done():
+			return
+		case <-ticker.C:
+			ticker.Reset(fingerprintPeriod)
+			ch <- d.buildFingerprint()
+		}
+	}
+}
+
+func (d *Driver) buildFingerprint() *drivers.Fingerprint {
+	var health drivers.HealthState
+	var desc string
+	attrs := map[string]*pstructs.Attribute{}
+
+	err := isInstalled()
+	version, vErr := systemdVersion()
+
+	if d.config.Enabled && err == nil && vErr == nil &&
+		driversUtil.IsUnixRoot() {
+		health = drivers.HealthStateHealthy
+		desc = "ready"
+		attrs["driver.nspawn"] = pstructs.NewBoolAttribute(true)
+		attrs["driver.nspawn.version"] = pstructs.NewStringAttribute(version)
+	} else {
+		health = drivers.HealthStateUndetected
+		desc = "disabled"
+	}
+
+	return &drivers.Fingerprint{
+		Attributes:        attrs,
+		Health:            health,
+		HealthDescription: desc,
+	}
+}
+
 func (d *Driver) RecoverTask(*drivers.TaskHandle) error {
 	return nil
 }
@@ -128,9 +210,59 @@ func (d *Driver) PluginInfo() (*base.PluginInfoResponse, error) {
 }
 
 func (d *Driver) ConfigSchema() (*hclspec.Spec, error) {
-	return nil, nil
+	return configSpec, nil
 }
 
 func (d *Driver) SetConfig(cfg *base.Config) error {
+	var config Config
+	if len(cfg.PluginConfig) != 0 {
+		if err := base.MsgPackDecode(cfg.PluginConfig, &config); err != nil {
+			return err
+		}
+	}
+
+	d.config = &config
+	if cfg.AgentConfig != nil {
+		d.nomadConfig = cfg.AgentConfig.Driver
+	}
+
 	return nil
+}
+
+func isInstalled() error {
+	_, err := exec.LookPath("systemd-nspawn")
+	if err != nil {
+		return err
+	}
+	_, err = exec.LookPath("machinectl")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// systemdVersion uses dbus to check which version of systemd is installed.
+func systemdVersion() (string, error) {
+	// check if systemd is running
+	if !systemdUtil.IsRunningSystemd() {
+		return "null", fmt.Errorf("systemd is not running")
+	}
+	bus, err := systemdDbus.NewSystemdConnection()
+	if err != nil {
+		return "null", err
+	}
+	defer bus.Close()
+	// get the systemd version
+	verString, err := bus.GetManagerProperty("Version")
+	if err != nil {
+		return "null", err
+	}
+	// lose the surrounding quotes
+	verNumString, err := strconv.Unquote(verString)
+	if err != nil {
+		return "null", err
+	}
+	// trim possible version suffix like in "242.19-1"
+	verNum := strings.Split(verNumString, ".")[0]
+	return verNum, nil
 }
